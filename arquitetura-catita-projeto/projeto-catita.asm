053c085792561efb0f5308b901be22164bc911b0
Davi Luiz <davilucena222@gmail.com>
2022-11-24 09:39:38 -0300

    Add files via upload

--- arquitetura-catita-projeto/projeto-catita.asm
+++ arquitetura-catita-projeto/projeto-catita.asm
@@ -15,7 +15,7 @@
 msgImgEntrada db "Digite o nome da imagem de entrada: ", 0H
 msgImgSaida db "Digite o nome da imagem de saida: ", 0H
 aumentoIntensidade db "Informe o valor de aumento da intensidade de cor da imagem: ",  0H
-selecaoCorBasica db "Informe uma das componentes básicas da imagem a ser alterada. Digite 0 para alterar a componente azul, digite 1 para alterar a componente verde ou 2 para alterar a componente vermelha: ", 0H
+selecaoCorBasica db "Informe uma das componentes basicas da imagem a ser alterada. Digite 0 para alterar a componente azul, digite 1 para alterar a componente verde ou 2 para alterar a componente vermelha: ", 0H
 newLine db 0AH
 inputString db 50 dup(0)
 outputString db 50 dup(0)
@@ -33,6 +33,8 @@ tam_outputString dd 0
 
 nomeImgEntrada db 50 dup(0)
 nomeImgSaida db 50 dup(0)
+intensidadeDaCor dd 0
+componenteBasica dd 0
  
 .code
 
@@ -59,7 +61,6 @@ invoke CreateFile, addr nomeImgEntrada, GENERIC_READ, 0, NULL, OPEN_EXISTING, FI
 mov fileHandle, eax
 
 invoke ReadFile, fileHandle, addr fileBuffer, 54, addr readCount, NULL
-invoke CloseHandle, fileHandle
 
 invoke WriteConsole, outputHandle, addr msgImgSaida, sizeof msgImgSaida, addr console_count, NULL 
 invoke ReadConsole, inputHandle, addr nomeImgSaida, sizeof nomeImgSaida, addr console_count, NULL 
@@ -78,15 +79,43 @@ invoke CreateFile, addr nomeImgSaida, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FIL
 mov fileHandleOut, eax
 
 invoke WriteFile, fileHandleOut, addr fileBuffer, 54, addr writeCount, NULL 
-invoke CloseHandle, fileHandleOut
 
 invoke CreateFile, addr nomeImgSaida, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL
 mov fileHandleOut, eax
 
 invoke ReadFile, fileHandleOut, addr fileBuffer2, 54, addr readCount, NULL
+invoke CloseHandle, fileHandleOut
 invoke CloseHandle, fileHandle
 
-printf("%08X\n", DWORD PTR[fileBuffer2+50])
+invoke WriteConsole, outputHandle, addr aumentoIntensidade, sizeof aumentoIntensidade, addr console_count, NULL
+invoke ReadConsole, inputHandle, addr intensidadeDaCor, sizeof intensidadeDaCor, addr console_count, NULL  
+
+ mov esi, offset intensidadeDaCor
+ proximo3:
+  mov al, [esi]
+  inc esi
+  cmp al, 13
+  jne proximo3
+  dec esi 
+  xor al, al 
+  mov [esi], al
+
+invoke WriteConsole, outputHandle, addr selecaoCorBasica, sizeof selecaoCorBasica, addr console_count, NULL
+invoke ReadConsole, inputHandle, addr componenteBasica, sizeof componenteBasica, addr console_count, NULL
+
+mov esi, offset componenteBasica
+ proximo4:
+  mov al, [esi]
+  inc esi
+  cmp al, 13
+  jne proximo4
+  dec esi 
+  xor al, al 
+  mov [esi], al
+
+
+;;printf("%d", intensidadeDaCor)
+;;printf("%08X\n", DWORD PTR[fileBuffer2+50])
 
 invoke ExitProcess, 0
 end start
